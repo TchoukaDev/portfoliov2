9a6f41e506b805861583458dc09f882ed9fe2631
Romain Wirth <romain.wirth@gmail.com>
2026-02-06 16:29:03 +0100

    style change color Quotde

--- src/Components/HomePage/Testimonial.tsx
+++ src/Components/HomePage/Testimonial.tsx
@@ -7,12 +7,12 @@ export default function Testimonial() {
     return (
         <Section alternate={true} className="text-center max-w-4xl mx-auto">
             <Container size="md">
-                <Quote className="text-blue-400 mb-4 -scale-x-100" />
+                <Quote className="text-blue-500 mb-4 -scale-x-100" />
                 <p className="text-lg md:text-xl text-gray-300 italic  max-w-2xl mx-auto">
 
                     Consciencieux, très disponible, toujours à la recherche du meilleur compromis, notre site est devenu très rapidement une référence dans notre spécialité !
                 </p>
-                <Quote className="text-blue-400 ml-auto mb-8" />
+                <Quote className="text-blue-500 ml-auto mb-8" />
                 <div >
                     <p className="text-white font-medium">Alain</p>
                     <p className="text-sm text-gray-500">
